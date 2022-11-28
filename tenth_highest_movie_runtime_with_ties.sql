7562632426a92739f0f57a325f4b8d51eb148ea1
Bamidele Adeola <ajamubamidele@gmail.com>
2022-11-28 16:06:52 +0100

    updated version

---
+++ tenth_highest_movie_runtime_with_ties.sql
@@ -0,0 +1,8 @@
+#Question
+-- Write the SQL query to get the 10th maximum movie runtime from the movies table.
+
+SELECT * FROM
+(SELECT title, genre, runtime,
+	DENSE_RANK() OVER(ORDER BY runtime DESC) AS runtime_ranks
+FROM movies) A
+WHERE runtime_ranks = 10;
