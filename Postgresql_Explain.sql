87e5f08f8d5337bdb64ae78a1bc8bb26d3930748
Ajamu Bamidele <ajamubamidele@gmail.com>
2023-08-13 22:24:03 +0100

    Postgresql explain

---
+++ Postgresql_Explain.sql
@@ -0,0 +1,22 @@
+-- Test Support
+
+-- SELECT, GROUP BY, FILTER
+
+--Select all columns from the film table
+SELECT * 
+FROM film;
+
+-- Explain the query performance of running all queries and just 2 columns
+EXPLAIN 
+SELECT * 
+FROM film;
+-- "Seq Scan on film  (cost=0.00..64.00 rows=1000 width=384)"
+
+
+
+--------
+EXPLAIN 
+SELECT title, release_year 
+FROM film
+LIMIT 10;
+-- "Seq Scan on film  (cost=0.00..64.00 rows=1000 width=19)"
